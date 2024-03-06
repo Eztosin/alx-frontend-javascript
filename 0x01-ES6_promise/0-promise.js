bfaefecbb2f0f42ba3da50b165c70cd656cc4b17
Eztosin <tosinezekiel03@gmail.com>
2024-03-06 19:30:24 +0000

    Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-	setTimeout(() => {
-	    resolve("API request Successful");
-	}, 3000);
-    });
-};
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('API request Successful');
+    }, 3000);
+  });
+}
