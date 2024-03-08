1219033cfa34026e652bf63078a4d3e7ce389ed6
Eztosin <tosinezekiel03@gmail.com>
2024-03-08 21:20:39 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default async function handleProfileSignup() {
   try {
     const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
-    console.log(`${photoResponse.body} ${userResponse.body.firstName} ${userResponse.body.lastName}`);
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
   } catch (error) {
     console.error('Signup system offline');
   }
