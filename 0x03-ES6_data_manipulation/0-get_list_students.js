6676d2ad6b3439049832292a5f0b6414ba653c38
Eztosin <tosinezekiel03@gmail.com>
2024-03-12 07:05:34 +0000

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,19 @@
 export default function getListStudents() {
-  return [
+  const objs = [
     { id: 1, firstName: 'Guillame', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+  for (const obj of objs) {
+    if (typeof obj.id !== 'number') {
+      throw new Error('Invalid type for id');
+    }
+    if (typeof obj.firstName !== 'string') {
+      throw new Error('Invalid type for firstName');
+    }
+    if (typeof obj.location !== 'string') {
+      throw new Error('Invalid type for location');
+    }
+  }
+  return objs;
 }
