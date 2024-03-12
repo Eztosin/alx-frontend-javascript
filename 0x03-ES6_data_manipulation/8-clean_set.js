c3f50a242e9ec61899e44994bb116527552c070f
Eztosin <tosinezekiel03@gmail.com>
2024-03-12 19:41:34 +0000

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
-    if (startString === '') {
-        return '';
-    }
-    return [...set].filter(value => value.startsWith(startString))
-                   .map(value => value.substring(startString.length))
-                   .join('-');
+  if (startString === '') {
+    return '';
+  }
+  return [...set].filter((value) => value.startsWith(startString))
+    .map((value) => value.substring(startString.length))
+    .join('-');
 }
