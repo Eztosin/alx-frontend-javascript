ed1623bd9500eb8666b886568037d11817c82892
Eztosin <tosinezekiel03@gmail.com>
2024-03-12 19:02:26 +0000

    Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-    if (position < 0 || position >= length) {
-	throw new Error('Position outside range');
-    }
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
 
-    const bufferLength = new ArrayBuffer(length);
-    const int8View = new Int8Array(bufferLength);
-    int8View[position] = value;
+  const bufferLength = new ArrayBuffer(length);
+  const int8View = new Int8Array(bufferLength);
+  int8View[position] = value;
 
-    return int8View;
+  return new DataView(bufferLength);
 }
