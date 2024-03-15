664ddf1f06f37bb186cf9fa4282a846e8d8ec74b
Eztosin <tosinezekiel03@gmail.com>
2024-03-15 21:24:22 +0000

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,44 +1,71 @@
 interface Teacher {
-readonly firstName: string;
-readonly lastName: string;
-fullTimeEmployee: boolean;
-yearsofExperience?: number;
-location: string;
-[key: string]: any;
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
 
 const teacher3: Teacher = {
-firstName: 'Oluwatosin',
-lastName: 'Ezekiel',
-fullTimeEmployee: false,
-location: 'Canada',
-contract: false,
+  firstName: 'Oluwatosin',
+  lastName: 'Ezekiel',
+  fullTimeEmployee: false,
+  location: 'Canada',
+  contract: false,
 };
 
+console.log(teacher3);
+
 interface Directors extends Teacher {
-numberOfReports: number;
+  numberOfReports: number;
 }
 
 const director1: Directors = {
-firstName: 'John',
-lastName: 'Doe',
-fullTimeEmployee: true,
-location: 'London',
-numberOfReports: 17,
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  location: 'London',
+  numberOfReports: 17,
 };
 
+console.log(director1);
+
 interface printTeacherFunction {
-(firstName: string, lastName: string): string;
+  (firstName: string, lastName: string): string;
 }
 
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
-const firstLetter = firstName.charAt(O).toUpperCase();
-const fullName = `${firstLetter}. ${lastName}`;
-return fullName;
+  const fullName = `${firstName.charAt(0).toUpperCase()}. ${lastName}`;
+  return fullName;
 };
 
 console.log(printTeacher("Joshua", "Doe"));
 
-console.log(teacher3);
-console.log(director1);
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
 
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
 
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
