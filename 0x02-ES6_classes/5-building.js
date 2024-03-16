74deac350d2a1cd40481ffb3d19c88a328986b5c
Eztosin <tosinezekiel03@gmail.com>
2024-03-16 05:32:48 +0000

    A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,14 +3,13 @@ export default class Building {
     if (typeof sqft !== 'number') {
       throw new TypeError('Squarefeet must be a number');
     }
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  static evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage'); // eslint-disable-line class-methods-use-this
   }
 }
