1f31b9e8407ffa3fcebbf5e4b4c7975a61628abd
Eztosin <tosinezekiel03@gmail.com>
2024-03-16 04:58:16 +0000

    Brand convention & Nominal typing

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,9 @@
+// Function sumMajorCredits
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return { credits: subject1.credits + subject2.credits, brand: 'MajorCredits' };
+}
+
+// Function sumMinorCredits
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return { credits: subject1.credits + subject2.credits, brand: 'MinorCredits' };
+}
